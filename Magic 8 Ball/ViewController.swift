6af6b6d7be1118f3618b17358d1269efb207f8c9
Ayaz Salikhov <asalikhov@aimtech.com>
2020-05-10 20:44:37 +0200

    Implement magic ball

--- Magic 8 Ball/ViewController.swift
+++ Magic 8 Ball/ViewController.swift
@@ -9,10 +9,17 @@
 import UIKit
 
 class ViewController: UIViewController {
+    @IBOutlet weak var imageView: UIImageView!
     
     let ballArray = [#imageLiteral(resourceName: "ball1.png"),#imageLiteral(resourceName: "ball2.png"),#imageLiteral(resourceName: "ball3.png"),#imageLiteral(resourceName: "ball4.png"),#imageLiteral(resourceName: "ball5.png")]
 
+    override func becomeFirstResponder() -> Bool {
+        return true
+    }
 
-
+    override func motionEnded(_ motion: UIEvent.EventSubtype, with event: UIEvent?){
+        if motion == .motionShake {
+            imageView.image = ballArray.randomElement()
+        }
+    }
 }
-
